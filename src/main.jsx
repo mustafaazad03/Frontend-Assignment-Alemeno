19c15d4bcc8dd4ebe762fc06e6d03313134145e7
Mustafa Azad <mustafaazad533@gmail.com>
2024-06-09 17:15:04 +0530

    chore: Update ESLint configuration and add Heroicons dependency

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import CourseDetail from "./pages/CourseDetail.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+		errorElement: <div>Home not found</div>,
+	},
+	{
+		// dynamic route for course detail page
+		path: "/course/:id",
+		element: <CourseDetail id="1" />,
+		errorElement: <div>Course not found</div>,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
