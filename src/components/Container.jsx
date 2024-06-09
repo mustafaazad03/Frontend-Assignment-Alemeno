c7a477c1d91364d5c98a33683caf9ef9a14561d7
Mustafa Azad <mustafaazad533@gmail.com>
2024-06-09 17:15:12 +0530

    chore: Update Container component styling

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 const Container = ({ children }) => {
 	return (
-		<div className="xl:max-w-7xl lg:max-w-4xl sm:max-w-lg max-w-[90%] mx-auto h-auto py-8">
+		<div className="xl:max-w-7xl lg:max-w-4xl max-w-[90%] mx-auto h-auto py-8 relative">
 			{children}
 		</div>
 	);
