f67853a06d79396e41ae3285341adf9d37140f8a
Mustafa Azad <mustafaazad533@gmail.com>
2024-06-09 17:15:30 +0530

    chore: Improve Navbar component with scroll behavior and fixed position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,33 @@
+import { useState, useEffect, useCallback } from "react";
 import searchIcon from "../assets/search.svg";
+
 const Navbar = () => {
+	const [showNavbar, setShowNavbar] = useState(true);
+	const [lastScrollY, setLastScrollY] = useState(0);
+
+	const handleScroll = useCallback(() => {
+		const currentScrollY = window.scrollY;
+		if (currentScrollY > lastScrollY) {
+			setShowNavbar(false);
+		} else {
+			setShowNavbar(true);
+		}
+		setLastScrollY(currentScrollY);
+	}, [lastScrollY]);
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, [handleScroll, lastScrollY]);
+
 	return (
-		<div className="flex items-center justify-between navbar--shadow md:mx-side p-4">
+		<div
+			className={`flex items-center justify-between navbar--shadow p-4 pb-2 px-[8%] fixed top-0 left-0 right-0 transition-transform duration-300 bg-white ${
+				showNavbar ? "transform translate-y-0" : "transform -translate-y-full"
+			}`}
+		>
 			<a href="/">
 				<h3 className="font-bold">Courses</h3>
 			</a>
@@ -14,7 +40,6 @@ const Navbar = () => {
 				/>
 				<img src={searchIcon} alt="search" className="absolute right-3" />
 			</div>
-			{/* Login and signup button in flex */}
 			<div className="flex items-center space-x-3 text-white">
 				<button
 					type="button"
