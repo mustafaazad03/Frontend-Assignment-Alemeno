c11139599bd95772f6a011ed47ae9c38d07e11f5
Mustafa Azad <mustafaazad533@gmail.com>
2024-06-11 10:52:14 +0530

    Refactor Container component to include Footer and improve page structure

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
 import Navbar from "./Common/Navbar";
+import Footer from "./Common/Footer";
 
 const Container = ({ children }) => {
 	return (
 		<div className="xl:max-w-7xl lg:max-w-4xl max-w-[90%] mx-auto h-auto py-8 relative">
 			<Navbar />
 			{children}
+			<Footer />
 		</div>
 	);
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import "./index.css";
-import Footer from "./components/Common/Footer";
 import { AppInitializer } from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<AppInitializer />
-			<Footer />
 		</Provider>
 	</React.StrictMode>
 );
